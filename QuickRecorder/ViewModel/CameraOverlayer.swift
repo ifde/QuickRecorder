cabaa7852081df444230e74029672e867fa79fe7
John <ifdeifde@gmail.com>
2025-02-28 08:01:11 +0300

    Flip button removed

--- QuickRecorder/ViewModel/CameraOverlayer.swift
+++ QuickRecorder/ViewModel/CameraOverlayer.swift
@@ -93,28 +93,6 @@ struct SwiftCameraView: View {
                 ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)) {
                     CameraView(type: type)
                         .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 0, y: 1, z: 0))
-                    Button(action: {
-                        if type == .idevice {
-                            for w in NSApp.windows.filter({ $0.title == "iDevice Overlayer".local }) { w.close() }
-                        } else {
-                            isFlipped.toggle()
-                        }
-                    }, label: {
-                        ZStack {
-                            Circle().frame(width: 30)
-                                .foregroundStyle(hover ? .blue : .gray)
-                            if type == .idevice {
-                                Image(systemName: "xmark")
-                                    .foregroundStyle(.white)
-                            } else {
-                                Image(systemName: "arrow.left.and.right.righttriangle.left.righttriangle.right.fill")
-                                    .foregroundStyle(.white)
-                                    .offset(y: -1)
-                            }
-                        }
-                        .opacity(hover ? 0.8 : 0.2)
-                        .onHover{ hovering in hover = hovering }
-                    }).buttonStyle(.plain).padding(10)
                 }.frame(width: geometry.size.width, height: geometry.size.height)
                 if SCContext.streamType == .window {
                     Text("Unable to use camera overlayer when recording a single window!".local
